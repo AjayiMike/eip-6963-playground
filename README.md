b1b509d28df53687b03aea255d6f4f6c26c771f0
AjayiMike <ajayimikeofficial@gmail.com>
2024-02-27 23:16:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite Starter kit
+
+This is a very simple React + TypeScript + Vite starterkit with tailwindcss and radix-ui setup
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@radix-ui/themes";
+
+function App() {
+    return (
+        <Box>
+            <h1 className="text-3xl font-bold">Hello world!</h1>
+        </Box>
+    );
+}
+
+export default App;
